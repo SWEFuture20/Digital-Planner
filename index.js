904595ceac6f6cbfed7cd467413dc6fd4128d697
Latifah Lillie <queen1892@gmail.com>
2021-02-12 19:57:36 -0600

    The time is displayed in the header w/ comment

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// let moment = require("moment");
 $(document).ready(function(){
 let timeDisplayed = document.querySelector("#currentDay");
 let time = moment().format('MMMM Do YYYY');
 console.log(time);
 
-
+function currentTime() {
+    timeDisplayed.append(time);
+}
+currentTime();
 })
-
-// function currentTime(){
-//    let time = Date.now() - moments;
-
-// }
-// currentTime();
