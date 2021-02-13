3b2ec117c35fc596212316958757a0a0ba46750d
Latifah Lillie <queen1892@gmail.com>
2021-02-12 19:50:30 -0600

    Time is Displayed in Console

---
+++ index.js
@@ -0,0 +1,14 @@
+// let moment = require("moment");
+$(document).ready(function(){
+let timeDisplayed = document.querySelector("#currentDay");
+let time = moment().format('MMMM Do YYYY');
+console.log(time);
+
+
+})
+
+// function currentTime(){
+//    let time = Date.now() - moments;
+
+// }
+// currentTime();
